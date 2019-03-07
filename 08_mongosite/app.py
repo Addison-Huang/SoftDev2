b1d0fa00b4e636580bd29106c201855b90d1a88c
ahuang20stuy <ahuang20@stuy.edu>
2019-03-06 22:41:44 -0500

    code

---
+++ 08_mongosite/app.py
@@ -0,0 +1,18 @@
+'''
+SoftDev2 pd8
+K 
+
+'''
+
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+
+@app.route("/")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
+
