0ecdbe817d716ddcd8c38ce49bf11f200f073618
ahuang20stuy <ahuang20@stuy.edu>
2019-04-14 20:42:43 -0400

    done

--- 17_listcomp/listcomp.py
+++ 17_listcomp/listcomp.py
@@ -1,6 +1,8 @@
-
 '''
 Team Zadical -- Zane Wang and Addison Huang
+SoftDev2 pd8
+K17 -- PPFTLCW
+2018-04-14
 '''
 
 def doubleEvenLoop():
@@ -44,12 +46,66 @@ def tripletsList():
 
 print(tripletsList())
 
+
 def compositeLoop():
     array = []
     for x in range(101):
-        for y in range(2,x/2):
-            if (x%y != 0):
+        for y in range(2,x/2+1):
+            if (x%y == 0):
                 array.append(x)
+                break;
+    return array
+print(compositeLoop())
+
+def compositeList():
+    return [x for x in range(101) if len([1 for y in range(2, x/2+1) if x % y == 0]) != 0]
+
+
+print(compositeList())
+
+def primeLoop():
+    array = []
+    composites = compositeLoop()
+    for x in range(2,101):
+        if not x in composites:
+            array.append(x)
     return array
 
-print(compositeLoop())
+print(primeLoop())
+        
+            
+def primeList():
+    return [x for x in range(2,101) if x not in compositeLoop()]
+
+print primeList()
+
+
+def divisorLoop(n):
+    array = []
+    for x in range(1,n):
+        if (n%x == 0):
+            array.append(x)
+    return array
+
+print(divisorLoop(15))
+
+def divisorList(n):
+    return [x for x in range(1, n) if n%x==0]
+
+print(divisorList(15))
+
+def transposeLoop(matrix):
+    array = []
+    for i in range(len(matrix[0])):
+        array.append([]) 
+        for j in range(len(matrix)):
+            array[i].append(matrix[j][i])
+    return array
+
+print(transposeLoop([[1,2,3],[4,5,6]]))
+
+
+def transposeList(matrix):
+        return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+print(transposeList([[1,2,3],[4,5,6]]))
