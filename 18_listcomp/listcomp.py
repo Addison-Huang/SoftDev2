dde74e76864e4b4a419543c3ada8c002d2ee641b
ahuang20stuy <ahuang20@stuy.edu>
2019-04-18 13:31:47 -0400

    lol

--- 18_listcomp/listcomp.py
+++ 18_listcomp/listcomp.py
@@ -6,7 +6,7 @@ k18
 '''
 
 def pythTriple(n):
-    return [[y,x,z] for x in range (1,n) for y in range(1,x) for z in range(1,n) if x**2 +y**2 == z**2]
+    return [(x,y,z) for x in range (1,n) for y in range(x,n) for z in range(y,n) if x**2 +y**2 == z**2]
 
 print(pythTriple(20))
 
