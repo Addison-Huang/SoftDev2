8a94041d819af8ac58c5559b64322c9bdb9158a7
ahuang20stuy <ahuang20@stuy.edu>
2019-05-01 22:24:31 -0400

    lol

---
+++ 23_closure/closure.py
@@ -0,0 +1,26 @@
+'''
+Addison Huang
+Softdev2 pd8
+K23 --closure
+01 05 2019
+'''
+
+def memoize(f):
+    memo={}
+    def helper(x):
+        if x not in memo.keys():
+            memo[x]=f(x)
+        return f(x)
+    return helper
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n ==1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+    
+
+fib = memoize(fib)
+print(fib(40))
